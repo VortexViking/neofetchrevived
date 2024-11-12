096a53eb6be91ce30b9bac5f8e4dc7810f004954
VorteViking <greenmonkey@duck.com>
2024-11-12 10:58:09 -0500

    notice

---
+++ notice.py
@@ -0,0 +1,8 @@
+#original repo https://github.com/dylanaraps/neofetch
+#this is a forked clone of the original repo. 
+#all code goes to the original author of the forked repo
+#this is just a preservation of the app to keep it up and running to the public
+#i will not be updating the code 
+# as stated the is just a preservation
+#if you wish to keep it up to date, become a contributer, or make your own fork
+#thank you 😃
